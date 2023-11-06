50bffa9df7be1af193006e4cbd6139b6bd39ab12
mahichy <mohiuddinoshim@yahoo.com>
2023-11-06 23:53:44 +0600

    add listings and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :listings
   get 'pages/about'
   get 'pages/contact'
+
+  root "listings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
