03e6e6826590ae4848f05bac4c84d1fd0f491bb9
mahichy <mohiuddinoshim@yahoo.com>
2023-11-20 23:46:30 +0600

    add validation for user and listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,7 @@ class Listing < ApplicationRecord
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "no_image.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  validates :name, :description, :price,  presence: true
+  validates :price, numericality: {greter_then: 0}
+
 end
